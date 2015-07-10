2566b2b5bba6d5285f114239dcedaff361152ed3
jiangbo12345 <251851509@qq.com>
2015-07-10 13:09:56 +0800

    update

--- src/com/yc/controller/init/InitDate.java
+++ src/com/yc/controller/init/InitDate.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Repository;
 
 import com.yc.entity.ShopCategory;
+import com.yc.entity.user.Personnel;
+import com.yc.entity.user.User;
+import com.yc.service.IPersonnelService;
 import com.yc.service.IShopCategoryService;
 
 @SuppressWarnings("rawtypes")
@@ -18,12 +22,24 @@ public class InitDate implements ApplicationListener{
 	@Resource
 	IShopCategoryService categoryService;
 	
+	@Autowired
+	IPersonnelService personnelService;
+	
 	@Override
 	public void onApplicationEvent(ApplicationEvent arg0) {
 		initArticleCategory();
 	}
 
 	private void initArticleCategory() {
+		Personnel  nat = personnelService.findById(1);
+		if (nat==null) {
+			nat = new Personnel();
+			nat.setUserName("管理员");
+			nat.setLoginName("admin");
+			nat.setPassword("000000");
+			nat.setForbidden(false);
+			personnelService.save(nat);
+		}
 		List<ShopCategory> categories = categoryService.getAllByParent();
 		if (categories==null || categories.size()==0) {
 			ShopCategory cate1 = new ShopCategory();
